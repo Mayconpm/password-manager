e94a6a5f368a730003bd64af801dcb53f8fbb026
maycon.marciano <pm.maycon@gmail.com>
2023-09-11 06:43:07 -0300

    Add function to save password and UI enhancements
    - Added a function to save the website, email, and password to a file.
    - Made UI enhancements by adding labels, input fields, buttons, and a logo image.
    - The user can now generate a password and add it to the password manager.
    - The changes improve the functionality and usability of the password manager.

--- main.py
+++ main.py
@@ -1,17 +1,62 @@
 import tkinter as tk
+from pathlib import Path
 
+PASSWORD_FILE = "passwords.txt"
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    website = website_input.get()
+    email = email_input.get()
+    password = password_input.get()
+    password_line = f"{website} | {email} | {password}\n"
+    path = Path(PASSWORD_FILE)
+
+    if path.is_file():
+        with open(PASSWORD_FILE, "a") as file:
+            file.write(password_line)
+    else:
+        with open(PASSWORD_FILE, "w") as file:
+            file.write(password_line)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = tk.Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20, width=200, height=200)
+window.config(padx=50, pady=50)
 
-canvas = tk.Canvas()
+canvas = tk.Canvas(width=200, height=200)
 canvas_logo_password = tk.PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=canvas_logo_password)
-canvas.grid(column=0, row=0)
+canvas.grid(column=1, row=0)
+
+website_label = tk.Label(text="Website: ")
+website_label.grid(column=0, row=1)
+
+email_label = tk.Label(text="Email/Username: ")
+email_label.grid(column=0, row=2)
+
+password_label = tk.Label(text="Password: ")
+password_label.grid(column=0, row=3)
+
+website_input = tk.Entry(width=52)
+website_input.focus()
+website_input.insert(0, "dummy.test@gmail.com")
+website_input.grid(column=1, row=1, columnspan=2)
+
+email_input = tk.Entry(width=52)
+email_input.grid(column=1, row=2, columnspan=2)
+
+password_input = tk.Entry(width=34)
+password_input.grid(column=1, row=3, sticky="e", padx=2)
+
+password_button = tk.Button(text="Generate Password")
+password_button.grid(column=2, row=3)
+
+add_button = tk.Button(text="Add", width=45, command=save_password)
+add_button.grid(column=1, row=4, columnspan=2)
+
+
 window.mainloop()
